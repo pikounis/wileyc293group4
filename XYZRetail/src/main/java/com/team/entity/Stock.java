57f576ce198e875d176380f9840b1134391441a7
fergus hodgson <41872563+fergus99@users.noreply.github.com>
2022-10-18 15:50:30 +0100

    Persistence layer

--- XYZRetail/src/main/java/com/team/entity/Stock.java
+++ XYZRetail/src/main/java/com/team/entity/Stock.java
@@ -1,5 +1,20 @@
 package com.team.entity;
 
-public class Stock {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Stock {
+	@Id
+	private int id;
+	private String stockType;
+	private double price;
+	private double TaxType;
+	private int Quantity;
 }
--- XYZRetail/src/main/java/com/team/entity/User.java
+++ XYZRetail/src/main/java/com/team/entity/User.java
@@ -1,5 +1,19 @@
 package com.team.entity;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+	@Id
+	private String username;
+	private String password;
+	private boolean admin;
 }
