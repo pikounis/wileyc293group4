ffca03431f075f6abf543d5ca7a5e74f1fef5076
fergus hodgson <41872563+fergus99@users.noreply.github.com>
2022-10-19 15:05:35 +0100

    remove last order from user object

---
+++ XYZRetail/src/main/java/com/team/persistence/ShoppingBasketDao.java
@@ -0,0 +1,7 @@
+package com.team.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoppingBasketDao extends JpaRepository<ShoppingBasket, Integer> {
+
+}
