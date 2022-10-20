514229f7859b6ed695442c9ac92f712d79bd83df
Cat6498 <cm.gtk9@gmail.com>
2022-10-20 18:58:29 +0200

    Made a displayobject

--- XYZRetail/src/main/java/com/team/controller/ShoppingBasketController.java
+++ XYZRetail/src/main/java/com/team/controller/ShoppingBasketController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.team.entity.DisplayItem;
 import com.team.entity.ShoppingBasketItem;
 import com.team.entity.User;
 import com.team.service.ShoppingBasketService;
@@ -22,7 +23,21 @@ public class ShoppingBasketController {
 	public ModelAndView getCartPage(@SessionAttribute("user") User user) {
 		ModelAndView view = new ModelAndView("ShoppingCart");
 		Collection<ShoppingBasketItem> prodList = basketService.getShoppingBasket(user);
-		view.addObject("products", prodList);
+		
+		Collection<DisplayItem> display = prodList.stream()
+				.map(item -> {
+					double price = item.getProduct().getProductPrice();
+					double tax = item.getProduct().getProductType().getTax();
+					int qty = item.getQuantity();
+					double totalPrice = (price*qty)+(tax/100)*qty*price;
+					return new DisplayItem(item.getProduct(), qty, item.getProduct().getProductType().getType(), price, tax, totalPrice);
+				}).toList();
+		double wholePrice = display.stream()
+			.map(DisplayItem::getWholePrice)
+			.reduce((a, b) -> a + b).orElse(null);
+				
+		view.addObject("total", wholePrice);
+		view.addObject("products", display);
 		return view;
 	}
 	
